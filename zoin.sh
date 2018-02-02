15bc8bac4c54da41ceb23eb9ba402d37a14981e6
dimasridho <34454470+dimasridho@users.noreply.github.com>
2018-02-02 23:49:21 +0700

    Update zoin.sh

--- zoin.sh
+++ zoin.sh
@@ -1,3 +1,3 @@
 apt-get --assume-yes update && apt-get --assume-yes upgrade && apt-get install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev && git clone https://github.com/zoin/cpuminer-zoin.git && cd cpuminer-zoin && ./build.sh && make && Sudo make install && screen
 cd cpuminer-zoin
-screen sudo ./cpuminer -a lyra2zoin -o stratum+tcp://zoi-pool3.chainsilo.com:3032 -u ignkuu.user1 -p x
+screen sudo ./cpuminer -a lyra2zoin -o stratum+tcp://zoi-pool3.chainsilo.com:3032 -u ignkuu.user -p x
