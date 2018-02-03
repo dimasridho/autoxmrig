38eca45b6041c909da4eebdfe6ae9d272f3e7323
dimasridho <34454470+dimasridho@users.noreply.github.com>
2018-02-03 11:05:55 +0700

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -4,13 +4,13 @@
 cd
 
 # server update & requesting apps install
-apt-get -y  install git build-essential cmake libuv1-dev libmicrohttpd-dev software-properties-common;
+apt-get -y install git build-essential cmake libuv1-dev libmicrohttpd-dev software-properties-common;
 
 # adding gcc repository
 add-apt-repository -y ppa:jonathonf/gcc-7.1;
 
 # reupdate source
-apt-get -y  update;
+apt-get -y update;
 
 # atcivate hugepages
 echo 10000 > /proc/sys/vm/nr_hugepages
@@ -25,7 +25,7 @@ echo vm.swappiness=10 > /etc/sysctl.conf
 sysctl -p
 
 # installing gcc
-apt-get -y  install gcc-7 g++-7;
+apt-get -y install gcc-7 g++-7;
 
 # cloning xmrig package
 git clone https://github.com/xmrig/xmrig.git
@@ -46,4 +46,3 @@ cmake .. -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7
 make
 cd xmrig/build
 screen ./xmrig -o etn.fairhash.org:3333 -u etnk5H17dxAQLhzExJxcBrFY8ZgPgTB6AH35cWTUFghGKseHgK1NSb429ZzRegQ3waQrbfcVaEiBoNoze7zVaGK72ftNxdhHsr -p x -t 1 -k --av=2 --donate-level=1
-
