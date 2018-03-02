d335afe8d97d9519412af437254a38c07e471a1a
dimasridho <34454470+dimasridho@users.noreply.github.com>
2018-03-02 21:54:22 +0700

    Update zoin.sh

--- zoin.sh
+++ zoin.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-apt-get -y update
-apt-get -y upgrade
-apt-get install -y git ca-certificates build-essential autoconf automake libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev
-git clone https://github.com/JayDDee/cpuminer-opt.git
-cd cpuminer-opt
-autoreconf -f -i -v && CFLAGS="-O3 -maes -mssse3 -mavx -mtune=intel -DUSE_ASM" CXXFLAGS="$CFLAGS -std=gnu++11" ./configure --with-curl && make -j8
+apt-get -y update && apt-get -y upgrade && apt-get -y install automake autoconf pkg-config libcurl4-openssl-dev libjansson-dev libssl-dev libgmp-dev && git clone https://github.com/zoin/cpuminer-zoin.git && cd cpuminer-zoin && ./build.sh && make && Sudo make install
 echo 10000 > /proc/sys/vm/nr_hugepages
 fallocate -l 4G /swapfile
 chmod 600 /swapfile
@@ -13,5 +8,4 @@ swapon /swapfile
 echo /swapfile > none    swap    sw    0   0 /etc/fstab
 echo vm.swappiness=10 > /etc/sysctl.conf
 sysctl -p
-cd cpuminer-opt
-screen
+cd cpuminer-zoin
