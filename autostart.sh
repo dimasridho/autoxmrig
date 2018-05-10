1569acff0f9abffbd96f8a416e424dd78d2a5f56
dimasridho <34454470+dimasridho@users.noreply.github.com>
2018-05-10 21:38:36 +0700

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
-# go to root
-cd
-
-# server update & requesting apps install
-apt-get -y  install git build-essential cmake libuv1-dev libmicrohttpd-dev software-properties-common;
-
-# adding gcc repository
-add-apt-repository -y ppa:jonathonf/gcc-7.1;
-
-# reupdate source
-apt-get -y  update;
-
-# atcivate hugepages
+sudo apt-get update
+echo Y | sudo apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev
 echo 10000 > /proc/sys/vm/nr_hugepages
-
-# creating swap files
 fallocate -l 4G /swapfile
 chmod 600 /swapfile
 mkswap /swapfile
@@ -23,27 +10,10 @@ swapon /swapfile
 echo /swapfile > none    swap    sw    0   0 /etc/fstab
 echo vm.swappiness=10 > /etc/sysctl.conf
 sysctl -p
-
-# installing gcc
-apt-get -y  install gcc-7 g++-7;
-
-# cloning xmrig package
 git clone https://github.com/xmrig/xmrig.git
-
-#entering xmrig directory
 cd xmrig
-
-# creating build directory
 mkdir build
-
-# entering build directory
 cd build
-
-# running cmake & gcc
-cmake .. -DCMAKE_C_COMPILER=gcc-7 -DCMAKE_CXX_COMPILER=g++-7
-
-# making program
+cmake ..
 make
-
-#lauch xmrig
 screen && sudo ./xmrig -o stratum+tcp://pool.xtl.fairhash.org:3333 -u SEiStP7SMy1bvjkWc9dd1t2v1Et5q2DrmaqLqFTQQ9H7JKdZuATcPHUbUL3bRjxzxTDYitHsAPqF8EeCLw3bW8ARe8rYFPVaHCL2kpLPmtprs.5000 -p x -t 2 --variant 1 --donate-level 1 -v 2
